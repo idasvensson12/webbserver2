0b4079e1f21d0e2a6f9d29e4535102bc7dccb5d6
idasvensson12 <54628205+idasvensson12@users.noreply.github.com>
2020-08-27 20:22:25 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+ let number5 = 5
+
+ let number10 = 10
+
+
+
+
+console.log(number5 + number10)
+
+
+console.log(number5 * number10)
+
+
+console.log(number5 - number10)
+
+
+console.log(number5 / number10)
